5ce789e112ba9192a3c86fde8a75a05cdd54498a
dir-FY24uenoyushin <yushin.ueno.x100@dirbato.co.jp>
2024-04-09 12:37:33 +0900

    変更

--- Sample/src/sample/Cast.java
+++ Sample/src/sample/Cast.java
@@ -3,8 +3,8 @@ package sample;
 public class Cast {
 
     public static void main(String[] args) {
-            var a = 3;
-            var b = 2;
+            int a = 3;
+            int b = 2;
 
             System.out.println( a / b );
             System.out.println( (double)a / b );
--- Sample/src/sample/Variable.java
+++ Sample/src/sample/Variable.java
@@ -3,7 +3,7 @@ package sample;
 public class Variable {
 
     public static void main(String[] args) {
-        var number1 = 1;
+        int number1 = 1;
         System.out.println(number1);
         
         String str1;
